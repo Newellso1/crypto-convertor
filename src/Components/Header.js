0d866242e212c9604f4c91e7e030ac5e8320aa7e
Sam Newell <samnewell91@gmail.com>
2024-09-23 16:30:58 +0100

    Header styling and depreciation updates

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="header bg-clip-text text-transparent bg-gradient-to-tr from-green-500 to-red-500 pt-5 text-center">
-      <h1 className="text-6xl">
+      <h1 className="text-6xl tracking-wider ">
         Crypto-<span className=" font-bold italic text-5xl">Chart</span>
       </h1>
     </div>
